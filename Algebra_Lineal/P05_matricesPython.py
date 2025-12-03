8cfa39f9b75caa351376a51ac729fddb2772d84c
Oscar Alfredo Pérez Lara <oscarinlara87@gmail.com>
2025-12-02 19:17:51 -0600

    Update P05_matricesPython.py

--- Algebra_Lineal/P05_matricesPython.py
+++ Algebra_Lineal/P05_matricesPython.py
@@ -1,7 +1,7 @@
 import numpy
 
 print("A en forma de lista de python")
-A = [["Alejandro", 4, 5], ["Javier", 5, 6]]
+A = [["Oscar", 4, 5], ["Lara", 5, 6]]
 print(A)
 print("A en forma matricial")
 A = numpy.array(A)
