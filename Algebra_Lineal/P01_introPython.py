5d05774514977085b53f6424ef79f043cbf5e013
Oscar Alfredo Pérez Lara <oscarinlara87@gmail.com>
2025-12-02 19:16:11 -0600

    Update P01_introPython.py

--- Algebra_Lineal/P01_introPython.py
+++ Algebra_Lineal/P01_introPython.py
@@ -2,18 +2,18 @@
 # EN PYTHON NO ES NECESARIO EL PUNTO Y COMA PARA TERMINAR INSTRUCCIONES
 
 print("Hola ");
-print("Alejandro");
+print("Oscar");
 print(",Cómo estas?");
 
 print("\r\n") #salto de linea \n = new line
 print("Hola ", end="");
-print("Alejandro", end="-");
+print("Oscar", end="-");
 print(",Cómo estas?");
 
 #Tipado dinamico!
 #El tipo de dato de una variable puede cambiar en tiempo de ejecucion
 #nombre  = 5
-nombre = "Alejandro"
+nombre = "Oscar"
 print("Hola " + str(nombre) + ", Cómo estas?") #casteo
 print("Hola", nombre,", Cómo estas?")
 
